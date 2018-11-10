fc840e1da2f796fa5b38706dc68a7c3efdbd9ebc
Geoffrey De Vylder <geoffreydv@gmail.com>
2018-11-10 13:05:50 +0100

    Added basics for html

--- web/src/main/java/be/geoffrey/xmlweave/web/controller/MainController.java
+++ web/src/main/java/be/geoffrey/xmlweave/web/controller/MainController.java
@@ -10,9 +10,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController {
 
     @GetMapping
-    @ResponseBody
     public String showMain() {
-        return "HELLO WORLD";
+        return "index";
     }
 
 }
